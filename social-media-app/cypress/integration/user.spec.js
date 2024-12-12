3e07ef6eccbee613ae4ccbe6ac399f8163dd8a97
GuilhermeCursino <guilhermecursino@hotmail.com>
2024-12-12 17:53:18 +0100

    uppdate 01

--- social-media-app/cypress/integration/user.spec.js
+++ social-media-app/cypress/integration/user.spec.js
@@ -1,18 +1,18 @@
 // cypress/integration/user.spec.js
 describe('User Registration and Login', () => {
   it('should register a new user', () => {
-    cy.visit('/register.html');
+    cy.visit('/user/register');
     cy.get('input[name="username"]').type('testuser');
     cy.get('input[name="password"]').type('password');
     cy.get('button[type="submit"]').click();
-    cy.contains('User registered');
+    cy.contains('User registered').should('be.visible');
   });
 
   it('should log in an existing user', () => {
-    cy.visit('/login.html');
+    cy.visit('/user/login.html');
     cy.get('input[name="username"]').type('testuser');
     cy.get('input[name="password"]').type('password');
     cy.get('button[type="submit"]').click();
-    cy.contains('Welcome to the protected page');
+    cy.contains('Welcome to the protected page').should('be.visible');
   });
 });
